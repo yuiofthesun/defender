9aaaf3bc05325cabadfcd9b9cd4d6f8967268899
Katharina Dankert <katharina.dankert@thoughtworks.com>
2019-05-22 21:29:14 +0200

    prettify

--- defender.py
+++ defender.py
@@ -1,30 +1,31 @@
 def horizontal_move(position, direction):
-    list1=enumerate(position)
+    list1 = enumerate(position)
     for i in list1:
         if i[1] is "@":
-            place=i[0]
+            place = i[0]
 
     def move(new_place, old_place):
         position[new_place] = "@"
         position[old_place] = "#"
-        result= ''.join(position)
+        result = ''.join(position)
         return result
 
     if direction == "right":
-        new_place=place+1
-        old_place=new_place-1
+        new_place = place + 1
+        old_place = new_place - 1
         return move(new_place, old_place)
 
     elif direction == "left":
-        new_place=place-1
-        old_place=new_place+1
+        new_place = place - 1
+        old_place = new_place + 1
         return move(new_place, old_place)
 
     else:
-        return  (''.join(position))
+        return ''.join(position)
+
 
 def main():
-    line =list(39*"#"+"@"+40*"#")
+    line = list(39 * "#" + "@" + 40 * "#")
     print(''.join(line))
     print(horizontal_move(line, "left"))
     print(horizontal_move(line, "right"))
