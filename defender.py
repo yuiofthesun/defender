083cafcf00a75b1ffefeaa9ed5afc1bfcbfe7bc4
Katharina Dankert <katharina.dankert@thoughtworks.com>
2019-05-22 21:16:48 +0200

    Refactored movement

--- defender.py
+++ defender.py
@@ -1,35 +1,33 @@
-def left(position):
+def horizontal_move(position, direction):
     list1=enumerate(position)
     for i in list1:
         if i[1] is "@":
             place=i[0]
 
-    new_place=place-1
+    def move(new_place, old_place):
+        position[new_place] = "@"
+        position[old_place] = "#"
+        result= ''.join(position)
+        return result
 
-    position[new_place] = "@"
-    position[new_place+1] = "#"
-    result= ''.join(position)
-    return result
+    if direction == "right":
+        new_place=place+1
+        old_place=new_place-1
+        return move(new_place, old_place)
 
-def right(position):
-    list1=enumerate(position)
-    for i in list1:
-        if i[1] is "@":
-            place=i[0]
+    elif direction == "left":
+        new_place=place-1
+        old_place=new_place+1
+        return move(new_place, old_place)
 
-    new_place=place+1
-
-    position[new_place] = "@"
-    position[new_place-1] = "#"
-    result= ''.join(position)
-    return result
-
+    else:
+        return  (''.join(position))
 
 def main():
     line =list(39*"#"+"@"+40*"#")
     print(''.join(line))
-    print(left(line))
-    print(right(line))
+    print(horizontal_move(line, "left"))
+    print(horizontal_move(line, "right"))
 
 
 if __name__ == '__main__':
